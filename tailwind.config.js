2970b68db3aefd9610af894984f9b9a7ccff9245
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-08-22 00:47:07 +0600

    navber design completed successfully

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,17 @@ module.exports = {
       colors: {
         primary: '#F4F4F4',
         secondary: '#ABEF5F',
+        subTitle: '#494949',
+        description: '#737373',
+        quinary: '#00B4D8',
+        senary: '#34495E',
+        septenary: '#E74C3C',
         
       },
       fontFamily: {
         manrope: ['Manrope', 'Arial', 'sans-serif'],
       },
+      container:{center:true}
     },
   },
   plugins: [
